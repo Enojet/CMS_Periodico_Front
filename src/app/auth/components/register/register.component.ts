4859705fe4ec0256572d6755098f6eef5fb68c9f
Cristian <cristianluisibanez@gmail.com>
2025-01-23 08:27:31 +0000

    feat: Implementación del formulario de registro con diseño mejorado

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerData = {
+    username: '',
+    role: '',
+    password: ''
+  };
 
+  onSubmit() {
+    // Aquí irá la lógica para enviar los datos al backend
+    console.log('Datos de registro:', this.registerData);
+  }
 }
