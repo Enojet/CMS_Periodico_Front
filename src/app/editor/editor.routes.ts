26d57d41f8dffb43c7506b630445b0ff592bb7b7
Miguel <misterbryn99@gmail.com>
2025-01-28 21:00:29 +0100

    Archivos Editor

--- src/app/editor/editor.routes.ts
+++ src/app/editor/editor.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ModifyArticleComponent } from './components/modify-article/modify-article.component';
+import { RevisableListComponent } from './components/revisable-list/revisable-list.component';
 
-export const EDITOR_ROUTES: Routes = [];
+export const EDITOR_ROUTES: Routes = [
+    {
+        path:"modify-article",
+        component:ModifyArticleComponent
+    },
+    {
+        path:"revisable-list",
+        component:RevisableListComponent
+    }
+];
+
+
--- src/app/editor/services/editor.service.ts
+++ src/app/editor/services/editor.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditorService {
 
-  constructor() { }
+  http: HttpClient = inject(HttpClient);
+
+  getAllArticles(){
+    return this.http.get("http://localhost:3000/articles")
+  }
 }
