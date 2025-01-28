03ab2038235470850aac6f9b07e05bb7073aa0f3
Cristian <cristianluisibanez@gmail.com>
2025-01-28 19:26:57 +0000

    actualizacion 1 login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-    {        
-        // Ruta principal de la aplicación: si el usuario no pone ruta, se va a /auth
-        // El redirectTo indica la ruta a la que se redirigirá
-        // El pathMatch indica que se debe hacer match con la ruta vacía (no con una ruta que comience con /)
-        path: '',
-        redirectTo: 'auth',  
-        pathMatch: 'full'
-    },
     {
         
         path: "auth",
@@ -31,10 +23,4 @@ export const routes: Routes = [
         loadChildren: () => import("./home/home.routes").then(module => module.HOME_ROUTES)
     },
 
-
-    {
-        // ruta por defecto: si el usuario no pone ruta, se va a /auth
-        path: "**",
-        redirectTo: "auth"
-    },
 ];
--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -8,13 +8,14 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
   // Usamos 'inject' para obtener una instancia del servicio HttpClient
   private http: HttpClient = inject(HttpClient)
+  private baseUrl = 'http://localhost:3600'; // URL base del backend
 
   // Método que maneja el registro de un usuario
   register(userForm: any) {
     // Realiza una solicitud HTTP POST al backend para registrar al usuario
     // Envía los datos del formulario 'userForm' al endpoint '/register' del servidor
     // OJO!!! PDTE REVISAR RUTA Y Nº PUERTO
-    return this.http.post("http://localhost:3000/register", userForm)
+    return this.http.post("http://localhost:3600/users/register", userForm)
     
   }
 
@@ -23,7 +24,7 @@ export class AuthService {
       // Realiza una solicitud HTTP POST al backend para logear al usuario
     // Envía los datos del formulario 'userLoginForm' al endpoint '/login' del servidor
     // OJO!!! PDTE REVISAR RUTA Y Nº PUERTO    
-    return this.http.post("http://localhost:3000/login", userLoginForm)
+    return this.http.post("http://localhost:3600/users/login", userLoginForm)
   }
 
   
